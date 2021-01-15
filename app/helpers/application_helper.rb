eadb17783b3e0839a8dc0b98a585dc6222531ff4
Arvin Fernandez <arvinf07@gmail.com>
2021-01-14 20:29:25 -0500

    Done.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def links
+    if session[:name]
+      form_tag ({controller: 'sessions', action: 'destroy', method: 'post'}) do 
+        submit_tag 'logout'
+      end
+    else
+      form_tag ({controller: 'sessions', action: 'create', method: 'post'}) do
+        submit_tag 'login'
+      end
+    end
+  end  
+
+
 end
